83b7c6f1f910b17fb6a02f4c1580f12a2df42ebc
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-26 15:31:01 +0000

    current date and time display added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var mainContentEl = $('#container-fluid')
+
 $(function () {
+  var timeNow = dayjs().format('ddd D YYYY, hh:mm:ss a');
+  var dateEl = $('#currentDay');
+  dateEl.text(timeNow);
+  var timeInterval = setInterval(function() {
+    timeNow = dayjs().format('ddd D YYYY, hh:mm:ss a');
+    dateEl.text(timeNow);
+}, 1000);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
