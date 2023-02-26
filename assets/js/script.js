cb9e58cb8178b0c9875ad054ed228d603df87a97
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-26 16:23:41 +0000

    rows for each hour are rendered on the page, with added styling depends on time (past,present,future), day planner set in 12h clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var mainContentEl = $('#container-fluid')
+var mainContentEl = $('#schedule');
+var dateEl = $('#currentDay');
 
-$(function () {
-  var timeNow = dayjs().format('ddd D YYYY, hh:mm:ss a');
-  var dateEl = $('#currentDay');
-  dateEl.text(timeNow);
-  var timeInterval = setInterval(function() {
-    timeNow = dayjs().format('ddd D YYYY, hh:mm:ss a');
-    dateEl.text(timeNow);
+// Display date and time on the page
+dateEl.text(dayjs().format('ddd D YYYY, hh:mm:ss a'));
+var timeInterval = setInterval(function() {
+  dateEl.text(dayjs().format('ddd D YYYY, hh:mm:ss a'));
 }, 1000);
+function changeTime(time) {
+  return (time+24) % 12 || 12;
+};
+$(function () {
+  for(var i=9; i<18; i++) {
+    console.log("i: " + i);
+    console.log(dayjs().format('H'));
+    var amPM = "PM";
+    var styleAt = "present";
+    var time = changeTime(i);
+    if(i < dayjs().format('H')) styleAt = "past";
+    else if(i > dayjs().format('H')) styleAt= "future";
+
+    if(i<12) amPM = "AM";
+    // past, present, future - classes to do style rows
+    var hourMainDivEl = $('<div id="" class="row time-block">');
+    var timeColEl = $('<div class="col-2 col-md-1 hour text-center py-3">'+ time + amPM + '</div>');
+    var textEl = $('<textarea class="col-8 col-md-10 description" rows="3">');
+    var btnEl = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">');
+    textEl.addClass(styleAt);
+
+    mainContentEl.append(hourMainDivEl);
+    hourMainDivEl.append(timeColEl);
+    hourMainDivEl.append(textEl);
+    hourMainDivEl.append(btnEl);
+    btnEl.append('<i class="fas fa-save" aria-hidden="true">');
+  };
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -30,3 +55,13 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+///////////////////////////////////////
+      // <div id="hour-9" class="row time-block past">
+      //   <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+      //   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      //   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+      //     <i class="fas fa-save" aria-hidden="true"></i>
+      //   </button>
+      // </div>
